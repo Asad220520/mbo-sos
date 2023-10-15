190ac1bfec8845e09fcc37e2352ac36bbbe057c2
aisalkyn3541 <126472043+aisalkyn3541@users.noreply.github.com>
2023-10-15 18:06:02 +0600

    add (#5)
    Co-authored-by: Aisalkyn <aisalkyn3541@gmail.com>

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,9 +1,11 @@
+import Fond from "./Fond";
 import Home from "./Home";
 import Programm from "./Programm";
 const HomePage = () => {
   return (
     <div>
       <Home />
+      <Fond/>
       <Programm />
     </div>
   );
