d39ac615a946fc8c787960d74ca1f601d6789ef6
Asad <130597420+Asad220520@users.noreply.github.com>
2023-10-21 11:15:54 +0600

    add (#10)
    Co-authored-by: Asadhkm <asadhkm4@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomePage from "./components/HomePage";
 import Gallery from "./components/Gallery";
 import Requisites from "./components/Requisites";
 import AboutFound from "./components/AboutFound";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,21 +1,21 @@
-// import AboutUs from "./AboutUs";
-// import Fond from "./Fond";
-// import Help from "./Help";
-// import Home from "./Home";
-// import Programm from "./Programm";
-// const HomePage = () => {
+import AboutUs from "./AboutUs";
+import Fond from "./Fond";
+import Help from "./Help";
+import Home from "./Home";
+import Programm from "./Programm";
+const HomePage = () => {
 
-//   return (
-//     <div>
-//       <Home />
-//       <Fond />
-//       <Programm />
-//       <AboutUs />
-//       <Help />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <Home />
+      <Fond />
+      <Programm />
+      <AboutUs />
+      <Help />
+    </div>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
 
 
